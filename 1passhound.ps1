c1de9930ba675c68435dd2ad8614016e7e1b45ac
Jared Atkinson <jatkinson@specterops.io>
2025-11-25 15:16:05 -0800

    Add PowerShell Help to 1PassHound functions

--- 1passhound.ps1
+++ 1passhound.ps1
@@ -1,5 +1,34 @@
+# TODO: Add documentation to each collector function regarding which 1Password permissions are necessary to collect the data
+
 function New-1PassHoundNode
 {
+    <#
+    .SYNOPSIS
+    Creates a 1PassHound Node object.
+    
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+    
+    This function creates a node object for use in 1PassHound's graph structure.
+
+    .PARAMETER Id
+    
+    The unique identifier for the node. This is derived from 1Password's internal IDs which are returned for each object from the CLI.
+    
+    .PARAMETER Kind
+
+    The type or category of the node (e.g., OPUser, OPGroup).
+
+    .PARAMETER Properties
+
+    A PSObject containing the properties of the node.
+
+    .EXAMPLE
+
+    $node = New-1PassHoundNode -Id "12345" -Kind "OPUser" -Properties $props
+
+    This example creates a new node of kind "OPUser" with the specified ID and properties.
+    #>
     Param(
         [Parameter(Position = 0, Mandatory = $true)]
         [String]
@@ -16,7 +45,7 @@ function New-1PassHoundNode
 
     $props = [pscustomobject]@{
         id = $Id
-        kinds = @($Kind, 'OPBase')
+        kinds = @($Kind)
         properties = $Properties
     }
 
@@ -25,12 +54,36 @@ function New-1PassHoundNode
 
 function New-1PassHoundItemNode
 {
+    <#
+    .SYNOPSIS
+    Creates a 1PassHound Item Node object. This is a subset of New-1PassHoundNode with additional kinds. Each item node will also have 'OPItem' and 'OPBase' kinds.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function creates a node object for use in 1PassHound's graph structure, specifically for item nodes. 
+    
+    Supported item node kinds are: OPApiCredential, OPCreditCard, OPDocument, OPLogin, OPPassport, OPPassword, OPSecureNote, OPServer, OPSoftwareLicense, OPSshKey, OPWirelessRouter.
+
+    .PARAMETER Id
+    The unique identifier for the item node. This is derived from 1Password's internal IDs which are returned for each object from the CLI.
+
+    .PARAMETER Kind
+    The specific type or category of the item node (e.g., OPLogin, OPCreditCard).
+
+    .PARAMETER Properties
+    A PSObject containing the properties of the item node.
+
+    .EXAMPLE
+    $itemNode = New-1PassHoundItemNode -Id "67890" -Kind "OPLogin" -Properties $itemProps
+    #>
     Param(
         [Parameter(Position = 0, Mandatory = $true)]
         [String]
         $Id,
 
         [Parameter(Position = 1, Mandatory = $true)]
+        [ValidateSet('OPApiCredential', 'OPCreditCard', 'OPDocument', 'OPLogin', 'OPPassport', 'OPPassword', 'OPSecureNote', 'OPServer', 'OPSoftwareLicense', 'OPSshKey', 'OPWirelessRouter')]
         [String]
         $Kind,
 
@@ -41,7 +94,7 @@ function New-1PassHoundItemNode
 
     $props = [pscustomobject]@{
         id = $Id
-        kinds = @($Kind, 'OPItem', 'OPBase')
+        kinds = @($Kind, 'OPItem')
         properties = $Properties
     }
 
@@ -50,6 +103,29 @@ function New-1PassHoundItemNode
 
 function New-1PassHoundEdge
 {
+    <#
+    .SYNOPSIS
+    Creates a 1PassHound Edge object.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function creates an edge object for use in 1PassHound's graph structure.
+
+    .PARAMETER Kind
+    The type or category of the edge (e.g., OPContains, OPMemberOf).
+
+    .PARAMETER StartId
+    The unique identifier of the starting node for the edge.
+
+    .PARAMETER EndId
+    The unique identifier of the ending node for the edge.
+
+    .EXAMPLE
+    $edge = New-1PassHoundEdge -Kind "OPContains" -StartId "12345" -EndId "67890"
+
+    This example creates a new edge of kind "OPContains" from the node with ID "12345" to the node with ID "67890".
+    #>
     Param(
         [Parameter(Position = 0, Mandatory = $true)]
         [String]
@@ -78,7 +154,27 @@ function New-1PassHoundEdge
     Write-Output $edge
 }
 
-function ConvertTo-PascalCase {
+function ConvertTo-PascalCase
+{
+    <#
+    .SYNOPSIS
+    Converts a given string to PascalCase format.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function takes a string input and converts it to PascalCase format, where the first letter of each word is capitalized and all words are concatenated without spaces or delimiters.
+
+    This function is used in 1PassHound to standardize permission names when creating edges in the graph structure.
+
+    .PARAMETER String
+    The input string to be converted to PascalCase.
+
+    .EXAMPLE
+    $pascalCaseString = ConvertTo-PascalCase -String "example_string-to_convert"
+
+    This example converts the input string "example_string-to_convert" to "ExampleStringToConvert".
+    #>
     param (
         [string]$String
     )
@@ -99,6 +195,15 @@ function ConvertTo-PascalCase {
 
 function Normalize-Null
 {
+    <#
+    .SYNOPSIS
+    Normalizes null values to empty strings.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+
+    #>
     param($Value)
     if ($null -eq $Value) { return "" }
     return $Value
@@ -106,6 +211,22 @@ function Normalize-Null
 
 function Get-1PassAccount
 {
+    <#
+    .SYNOPSIS
+    Retrieves and constructs the 1PassHound node for the 1Password account.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    The 1Password Account node represents the overall account in 1PassHound's graph structure. This function fetches account details using the 1Password CLI and constructs a node with relevant properties.
+
+    It is built on top of the `op account get` and `op account list` commands to gather account information.
+
+    .EXAMPLE
+    $accountNode = Get-1PassAccount
+
+    This example retrieves the 1Password account information and returns it as a node object.
+    #>
     $nodes = New-Object System.Collections.ArrayList
 
     $account = op account get --format json | ConvertFrom-Json
@@ -127,6 +248,22 @@ function Get-1PassAccount
 
 function Get-1PassUser
 {
+    <#
+    .SYNOPSIS
+    Retrieves and constructs 1PassHound nodes and edges for 1Password users.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function fetches user details from the 1Password account using the 1Password CLI and constructs nodes and edges representing users in 1PassHound's graph structure.
+
+    It is built on top of the `op user list` command to gather user information.
+
+    .EXAMPLE
+    $userGraph = Get-1PassUser
+
+    This example retrieves the list of users in the 1Password account and returns them as nodes and edges.
+    #>
     $nodes = New-Object System.Collections.ArrayList
     $edges = New-Object System.Collections.ArrayList
 
@@ -161,6 +298,22 @@ function Get-1PassUser
 # This requires some level of permission on the group because I cannot see the role information on most groups
 function Get-1PassGroup
 {
+    <#
+    .SYNOPSIS
+    Retrieves and constructs 1PassHound nodes and edges for 1Password groups.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function fetches group details from the 1Password account using the 1Password CLI and constructs nodes and edges representing groups in 1PassHound's graph structure.
+
+    It is built on top of the `op group list` and `op group get` commands to gather group information.
+
+    .EXAMPLE
+    $groupGraph = Get-1PassGroup
+
+    This example retrieves the list of groups in the 1Password account and returns them as nodes and edges.
+    #>
     $nodes = New-Object System.Collections.ArrayList
     $edges = New-Object System.Collections.ArrayList
 
@@ -232,6 +385,22 @@ function Get-1PassGroup
 
 function Get-1PassVault
 {
+    <#
+    .SYNOPSIS
+    Retrieves and constructs 1PassHound nodes and edges for 1Password vaults.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function fetches vault details from the 1Password account using the 1Password CLI and constructs nodes and edges representing vaults in 1PassHound's graph structure.
+
+    It is built on top of the `op vault list` and `op vault get` commands to gather vault information.
+
+    .EXAMPLE
+    $vaultGraph = Get-1PassVault
+
+    This example retrieves the list of vaults in the 1Password account and returns them as nodes and edges.
+    #>
     $nodes = New-Object System.Collections.ArrayList
     $edges = New-Object System.Collections.ArrayList
 
@@ -295,6 +464,26 @@ function Get-1PassVault
 
 function Get-1PassItem
 {
+    <#
+    .SYNOPSIS
+    Retrieves and constructs 1PassHound nodes and edges for 1Password items.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function fetches item details from the 1Password account using the 1Password CLI and constructs nodes and edges representing items in 1PassHound's graph structure.
+
+    It is built on top of the `op item list` command to gather item information.
+
+    It currently excludes items in the 'Employee' vault. This can be modified as needed, but is intended to reduce noise from personal items.
+
+    Additionally, 1PassHound will only see the Employee vault for the user running the script.
+
+    .EXAMPLE
+    $itemGraph = Get-1PassItem
+
+    This example retrieves the list of items in the 1Password account and returns them as nodes and edges.
+    #>
     $nodes = New-Object System.Collections.ArrayList
     $edges = New-Object System.Collections.ArrayList
 
@@ -361,6 +550,20 @@ function Get-1PassItem
 
 function Invoke-1PassHound
 {
+    <#
+    .SYNOPSIS
+    Main function to execute 1PassHound enumeration and generate the output JSON file.
+
+    .DESCRIPTION
+    Author: Jared Atkinson (@cobbler) at SpecterOps
+
+    This function orchestrates the enumeration of the 1Password account, users, groups, vaults, and items. It collects all nodes and edges into a single graph structure and outputs it as a JSON file.
+
+    The output file name follows the format `1pass_<account_id>.json`.
+
+    .EXAMPLE
+    Invoke-1PassHound
+    #>
     $edges = New-Object System.Collections.ArrayList
     $nodes = New-Object System.Collections.ArrayList
 
